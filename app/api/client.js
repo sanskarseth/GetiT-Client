75e9119307b7c7bdbca4d13a2578de65a2ffcfce
Sanskar <sethsanskar@gmail.com>
2021-04-20 17:41:19 +0530

    updated name

--- app/api/client.js
+++ app/api/client.js
@@ -1,29 +1,33 @@
-import { create } from "apisauce";
-import cache from "../utility/cache";
-import authStorage from "../auth/storage";
-import settings from "../config/settings";
+import { create } from 'apisauce';
+import cache from '../utility/cache';
+import authStorage from '../auth/storage';
+import settings from '../config/settings';
 
 const apiClient = create({
-  baseURL: settings.apiUrl,
+	baseURL: settings.apiUrl,
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json',
+	},
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
-  const authToken = await authStorage.getToken();
-  if (!authToken) return;
-  request.headers["x-auth-token"] = authToken;
+	const authToken = await authStorage.getToken();
+	if (!authToken) return;
+	request.headers['x-auth-token'] = authToken;
 });
 
 const get = apiClient.get;
 apiClient.get = async (url, params, axiosConfig) => {
-  const response = await get(url, params, axiosConfig);
+	const response = await get(url, params, axiosConfig);
 
-  if (response.ok) {
-    cache.store(url, response.data);
-    return response;
-  }
+	if (response.ok) {
+		cache.store(url, response.data);
+		return response;
+	}
 
-  const data = await cache.get(url);
-  return data ? { ok: true, data } : response;
+	const data = await cache.get(url);
+	return data ? { ok: true, data } : response;
 };
 
 export default apiClient;
